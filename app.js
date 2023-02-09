7edcd5c62a7b04cc86e0bd8e6dc5364550cfad1c
VL_1062 <vlch8100@gmail.com>
2023-02-09 16:20:17 +0530

    authentication is done to doctor

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-
-// require('./connection/connection')
-// const express = require('express')
-// const DoctorRouter = require('./routers/doctor-routes')
-// const PatientRouter = require('./routers/patient-routes')
-// const app = express();
-// app.use(express.json())
-// app.use(DoctorRouter);
-// app.use(PatientRouter);
-// app.listen(3001, () => {
-//     console.log("server running on port 3001");
-//   // console.log('Sc:\Users\LENOVO\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\code\electron-sandbox\workbench\workbench.htmlerver running on port 3001');
-//   });
+// const middleware=require('./middleware/authentication')
 const express = require('express')
 const mongoose = require('mongoose')
 const url = 'mongodb+srv://database:Database@cluster0.g8vaoci.mongodb.net/test'
@@ -29,29 +17,12 @@ mongoose.set('strictQuery', false);
 
 // checking if account is changed or not 
 
-// <<<<<<< HEAD
-// MONGODB_URI = 'mongodb+srv://database:Database@cluster0.g8vaoci.mongodb.net/test'
-// =======
-// MONGODB_URI = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2'
-// >>>>>>> development
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// const bodyParser = require('body-parser');
-
-// app.use((req, res, next) => {
-//   if (req.method === 'POST' || req.method === 'PUT') {
-//     app.use(bodyParser.json());
-//     app.use(bodyParser.urlencoded({ extended: true }));
-//   }
-//   next();
-// });
-
 
 
 app.use(doctorRouter)
@@ -64,11 +35,6 @@ mongoose.connect(url,{ useNewUrlParser: true },(err)=>{
 else{
     console.log("successfully DB connected")
 }})
-
-
-
-
-
 
 
 app.get('/',(req,res)=>{
--- controllers/doctorControllers.js
+++ controllers/doctorControllers.js
@@ -1,27 +1,12 @@
 const Doctor = require('../models/doctormodels')
 const Patient = require('../models/patientmodel')
+const jwt=require('jsonwebtoken')
 
-
-const getAllDoctors = async (req,res,next) => {
-    let doctors;
-    try{
-        doctors = await Doctor.find()
-        console.log("get updated doctors")
-        
-    }
-    catch(err){
-        console.log(err)
-    }
-    if(!doctors){
-        return res.status(404).json({message:"no doctor found"})
-    }
-    return res.status(200).json({doctors})
-}
 
 
 const addDoctors = async(req,res,next) => {
     let doctor =req.body
-    // console.log(doctor)
+    console.log(doctor)
     // console.log(doctor.name)
     let existingdoctor
     try{
@@ -42,6 +27,63 @@ const addDoctors = async(req,res,next) => {
         return console.log(err)
      }
      return res.status(201).json({doctor})
+}
+const loginDoctors=async(req,res,next)=>{
+    try{
+        const {email,password} = req.body;
+        let exist = await Doctor.findOne({email});
+        if(!exist) {
+            return res.status(400).send('User Not Found');
+        }
+        if(exist.password !== password) {
+            return res.status(400).send('Invalid credentials');
+        }
+        let payload = {
+            user:{
+                id : exist.id
+            }
+        }
+        jwt.sign(payload,'jwtSecret',{expiresIn:3600000},
+          (err,token) =>{
+              if (err) throw err;
+              return res.json({token})
+          }  
+            )
+
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).send('Server Error')
+    }
+}
+const doctordashboard= async (req,res) =>{
+    try{
+        let exist = await Doctor.findById(req.user.id);
+        if(!exist){
+            return res.status(400).send('User not found');
+        }
+        res.json(exist);
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).send('Server Error')
+    }
+}
+
+const getAllDoctors = async (req,res,next) => {
+  let doctors;
+  try{
+      doctors = await Doctor.find()
+      console.log("get updated doctors")
+      
+  }
+  catch(err){
+      console.log(err)
+  }
+  if(!doctors){
+      return res.status(404).json({message:"no doctor found"})
+  }
+  return res.status(200).json({doctors})
 }
 
 const updateDoctors = async(req,res,next)=>{
@@ -94,4 +136,4 @@ const getDoctorById = async (req,res,next) => {
     return res.status(200).json({doctor})
 }
 
-module.exports={getAllDoctors , getDoctorById ,addDoctors ,updateDoctors,deleteDoctor}
+module.exports={getAllDoctors , getDoctorById ,addDoctors ,updateDoctors,deleteDoctor,loginDoctors,doctordashboard}
